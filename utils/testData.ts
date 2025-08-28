89954db10724243938e9a1d91b4d1991fbd1f45c
NirozShah <shah.nairoz@gmail.com>
2025-08-28 07:32:57 -0400

    Test Data file commit
    Test Data file

---
+++ utils/testData.ts
@@ -0,0 +1,20 @@
+export const users = {
+username: "standard_user",
+password:"secret_sauce",
+};
+
+export const products = {
+  item1: "Sauce Labs Backpack",
+  item2: "Sauce Labs Bike Light",
+  itemNames: ["Sauce Labs Backpack", "Sauce Labs Bike Light"]
+};
+
+export const checkoutDetails = {
+  firstName: "John",
+  lastName: "Williams",
+  postalCode: "12345"
+};
+
+export const PageURL = {
+  Url : "https://www.saucedemo.com/",
+};
